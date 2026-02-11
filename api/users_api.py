3b50f074674f7fea61fb32bf06c3c7c75f9fca5c
Ruslan Bakhtiev <rootpravil066@gmail.com>
2026-02-11 15:43:16 +0300

    corrected users paths

--- api/users_api.py
+++ api/users_api.py
@@ -1,17 +1,17 @@
 from fastapi import APIRouter, HTTPException
 from schemas.users_schema import User
 from api.depencies import DBDep
-
+from services.user_service import UserService
 
 router = APIRouter(prefix="/user", tags=["Пользователь"])
 
 
 @router.get("/me", description="получить информацию о себе")
 async def get_about_user(db: DBDep):
-    res = await db.user.get_one_or_none()
-    if res:
-        return {"status":"OK", "data": res}
-    raise HTTPException(status_code=404, detail="user not found")
+    try:
+        return await UserService(db).get_me()
+    except:
+        raise HTTPException(status_code=404, detail="user not found")
 
 
 @router.post("/auth")
@@ -19,10 +19,9 @@ async def auth_user():
     pass
 
 
-@router.get("", description="получить всех пользователей")
+@router.get("/", description="получить всех пользователей")
 async def get_all_users(db: DBDep):
-    res = await db.user.get_all()
-    return {"status": "OK", "data": res}
+    return UserService(db).get_all()
 
 
 @router.post("/register", description="регистрация нового пользователя")
@@ -32,16 +31,16 @@ async def register_user(user_data: User, db: DBDep):
     return {"status": "OK", "new_data": user}
 
 
-@router.delete("", description="удаление пользователя по ид")
+@router.delete("/", description="удаление пользователя по ид")
 async def delete_user(user_id: int):
     pass
 
 
-@router.put("", description="полное обновление пользователя")
+@router.put("/", description="полное обновление пользователя")
 async def put_user(user_data: User):
     pass
 
 
-@router.patch("", description="частичное обновление пользователя")
+@router.patch("/", description="частичное обновление пользователя")
 async def patch_user(user_data: User):
     pass
