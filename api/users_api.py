aad0889586d5f03d204f87126b601ac5cf9e50dc
Ruslan Bakhtiev <rootpravil066@gmail.com>
2026-02-11 11:58:45 +0300

    refactor for db depends

--- api/users_api.py
+++ api/users_api.py
@@ -1,18 +1,17 @@
 from fastapi import APIRouter, HTTPException
-from database import async_session
-from repos.users_repos import UsersRepository
 from schemas.users_schema import User
+from api.depencies import DBDep
+
 
 router = APIRouter(prefix="/user", tags=["Пользователь"])
 
 
 @router.get("/me", description="получить информацию о себе")
-async def get_about_user():
-    async with async_session() as session:
-        res = await UsersRepository(session).get_one_or_none()
-        if res:
-            return {"status":"OK", "data": res}
-        raise HTTPException(status_code=404, detail="user not found")
+async def get_about_user(db: DBDep):
+    res = await db.user.get_one_or_none()
+    if res:
+        return {"status":"OK", "data": res}
+    raise HTTPException(status_code=404, detail="user not found")
 
 
 @router.post("/auth")
@@ -21,18 +20,16 @@ async def auth_user():
 
 
 @router.get("", description="получить всех пользователей")
-async def get_all_users():
-    async with async_session() as session:
-        res = await UsersRepository(session).get_all()
-        return {"status": "OK", "data": res}
+async def get_all_users(db: DBDep):
+    res = await db.user.get_all()
+    return {"status": "OK", "data": res}
 
 
 @router.post("/register", description="регистрация нового пользователя")
-async def register_user(user_data: User):
-    async with async_session() as session:
-        user = await UsersRepository(session).insert_data(user_data)
-        await session.commit()
-        return {"status": "OK", "new_data": user}
+async def register_user(user_data: User, db: DBDep):
+    user = db.user.insert_data(user_data)
+    await db.commit()
+    return {"status": "OK", "new_data": user}
 
 
 @router.delete("", description="удаление пользователя по ид")
