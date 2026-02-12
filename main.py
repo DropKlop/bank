f33a957a4c26b955c3c0011b55c2cfcb92daa2de
Ruslan Bakhtiev <rootpravil066@gmail.com>
2026-02-12 17:45:19 +0300

    update gitignore

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from pathlib import Path
 
 sys.path.append(str(Path(__file__).parent.parent))
 
-
 from api.users_api import router as user_router
 
 
@@ -14,7 +13,6 @@ from api.users_api import router as user_router
 app = FastAPI()
 
 app.include_router(user_router)
-
 
 if __name__ == "__main__":
     uvicorn.run(app="main:app", host="localhost", port=8000, reload=True)
