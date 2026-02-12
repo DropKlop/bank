9159aceeef57b2555b29685c9b56808e1684dbc2
Ruslan Bakhtiev <rootpravil066@gmail.com>
2026-02-12 11:39:38 +0300

    update schemas

--- services/user_service.py
+++ services/user_service.py
@@ -1,6 +1,29 @@
 from services.base import BaseService
+from schemas.users_schema import UserAdd, UserPatch
+
 
 class UserService(BaseService):
     async def get_me(self):
-        res = await self.db.user.get_one_or_none()
+        res = await self.db.user.get_one()
         return {"status": "OK", "data": res}
+
+    async def get_all(self, limit, offset):
+        res = await self.db.user.get_filtered(limit=limit, offset=offset)
+        return {"status": "OK", "data": res}
+
+    async def insert_data(self, data: UserAdd):
+        user = await self.db.user.insert_data(data)
+        await self.db.commit()
+        return {"status": "OK", "new_data": user}
+
+    async def delete_data(self, user_id: int):
+        await self.db.user.delete_data(id=user_id)
+        await self.db.commit()
+
+    async def patch_data(self, user_id: int, data: UserPatch):
+        await self.db.user.update_data(data, True, id=user_id)
+        await self.db.commit()
+
+    async def put_data(self, user_id: int, data: UserAdd):
+        await self.db.user.update_data(data, False, id=user_id)
+        await self.db.commit()
